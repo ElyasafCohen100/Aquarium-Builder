38e1559e1a945b11e7c68b3aef593a1fa5ac39be
Elyasaf Cohen <Elyasaf93@gmail.com>
2026-02-01 05:35:18 +0200

    feat: initial frontend setup with React

---
+++ AquariumBuilder.Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h2>Welcome to Aquarium Builder 🐠</h2>
+        <p>Here we will build and manage our aquarium step by step</p>
+      </main>
+    </>
+  );
+}
