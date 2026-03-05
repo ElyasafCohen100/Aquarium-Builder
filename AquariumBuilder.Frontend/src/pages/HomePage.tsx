64a6c6215ecf32bdf122b85e2991b4ffbefee978
Elyasaf Cohen <Elyasaf93@gmail.com>
2026-03-05 22:47:43 +0200

    feat: initialize frontend folder structure and layout skeleton

---
+++ AquariumBuilder.Frontend/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <>
+            <h2>Welcome to Aquarium Builder 🐠</h2>
+            <p>Here we will build and manage our aquarium step by step</p>
+        </>
+    );
+};
+
+export default HomePage;
